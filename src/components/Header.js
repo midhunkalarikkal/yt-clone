5d397fd74709f6ab189e2e636520e3e08bac15d8
midhunkalarikkal <91378594+midhunkalarikkal@users.noreply.github.com>
2024-11-06 00:36:35 +0530

    structure cleaned tailwind installed and configures basic structure created, hamburger and logo imported

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div>
+        <div>
+            <img src='/icons/hamburgerBlack.png' alt='hamburger' />
+        </div>
+    </div>
+  )
+}
+
+export default Header
