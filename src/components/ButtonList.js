d0bac47b5b4c0b4c73158a2a9996c943fb1c225a
midhunkalarikkal <91378594+midhunkalarikkal@users.noreply.github.com>
2024-11-06 16:29:04 +0530

    SubHeader designed

--- src/components/ButtonList.js
+++ src/components/ButtonList.js
@@ -1,9 +1,34 @@
-import React from 'react'
+import React from "react";
 
 const ButtonList = () => {
+  const buttonNames = [
+    "All",
+    "Live",
+    "Cinema",
+    "News",
+    "APIs",
+    "Music",
+    "Web pages",
+    "Google",
+    "Gaming",
+    "Tech",
+    "Shows",
+    "Sports",
+    "TV Shows",
+    "Trailer",
+    "Comedy",
+    "Space",
+    "Cooking",
+    "Travel",
+    "Car",
+  ];
   return (
-    <div>ButtonList</div>
-  )
-}
+    <div className="p-4 overflow-x-scroll whitespace-nowrap no-scrollbar">
+      {buttonNames.map((button, index) => (
+        <button key={index}className="px-4 py-1 rounded-lg bg-[#f0f0f0] mx-1">{button}</button>
+      ))}
+    </div>
+  );
+};
 
-export default ButtonList
+export default ButtonList;
--- src/components/MainContainer.js
+++ src/components/MainContainer.js
@@ -4,7 +4,7 @@ import VideoContainer from './VideoContainer'
 
 const MainContainer = () => {
   return (
-    <div>
+    <div className='w-[86%]'>
         <ButtonList />
         <VideoContainer />
     </div>
