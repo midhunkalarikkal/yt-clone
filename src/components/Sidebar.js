f987f7ac31ece157740da29071b0c5261c031b24
midhunkalarikkal <91378594+midhunkalarikkal@users.noreply.github.com>
2024-11-06 15:35:10 +0530

    Side bar designed and made it y scroll

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,101 +1,205 @@
-import React from 'react'
-import HomeIcon from '@mui/icons-material/Home';
-import VideoStableOutlinedIcon from '@mui/icons-material/VideoStableOutlined';
-import SubscriptionsOutlinedIcon from '@mui/icons-material/SubscriptionsOutlined';
-import KeyboardArrowRightOutlinedIcon from '@mui/icons-material/KeyboardArrowRightOutlined';
-import HistoryOutlinedIcon from '@mui/icons-material/HistoryOutlined';
-import PlaylistPlayOutlinedIcon from '@mui/icons-material/PlaylistPlayOutlined';
-import SmartDisplayOutlinedIcon from '@mui/icons-material/SmartDisplayOutlined';
-import WatchLaterOutlinedIcon from '@mui/icons-material/WatchLaterOutlined';
-import ThumbUpOutlinedIcon from '@mui/icons-material/ThumbUpOutlined';
-import DeblurRoundedIcon from '@mui/icons-material/DeblurRounded';
-import WhatshotIcon from '@mui/icons-material/Whatshot';
-import LocalMallOutlinedIcon from '@mui/icons-material/LocalMallOutlined';
-import MusicNoteOutlinedIcon from '@mui/icons-material/MusicNoteOutlined';
-import MovieCreationOutlinedIcon from '@mui/icons-material/MovieCreationOutlined';
-import LiveTvOutlinedIcon from '@mui/icons-material/LiveTvOutlined';
-import SportsEsportsOutlinedIcon from '@mui/icons-material/SportsEsportsOutlined';
-import NewspaperOutlinedIcon from '@mui/icons-material/NewspaperOutlined';
-import EmojiEventsOutlinedIcon from '@mui/icons-material/EmojiEventsOutlined';
-import LightbulbOutlinedIcon from '@mui/icons-material/LightbulbOutlined';
-import DiamondOutlinedIcon from '@mui/icons-material/DiamondOutlined';
-import PodcastsOutlinedIcon from '@mui/icons-material/PodcastsOutlined';
-import SettingsOutlinedIcon from '@mui/icons-material/SettingsOutlined';
-import OutlinedFlagOutlinedIcon from '@mui/icons-material/OutlinedFlagOutlined';
-import HelpOutlineOutlinedIcon from '@mui/icons-material/HelpOutlineOutlined';
-import AnnouncementOutlinedIcon from '@mui/icons-material/AnnouncementOutlined';
+import React from "react";
+import HomeIcon from "@mui/icons-material/Home";
+import VideoStableOutlinedIcon from "@mui/icons-material/VideoStableOutlined";
+import SubscriptionsOutlinedIcon from "@mui/icons-material/SubscriptionsOutlined";
+import KeyboardArrowRightOutlinedIcon from "@mui/icons-material/KeyboardArrowRightOutlined";
+import HistoryOutlinedIcon from "@mui/icons-material/HistoryOutlined";
+import PlaylistPlayOutlinedIcon from "@mui/icons-material/PlaylistPlayOutlined";
+import SmartDisplayOutlinedIcon from "@mui/icons-material/SmartDisplayOutlined";
+import WatchLaterOutlinedIcon from "@mui/icons-material/WatchLaterOutlined";
+import ThumbUpOutlinedIcon from "@mui/icons-material/ThumbUpOutlined";
+import DeblurRoundedIcon from "@mui/icons-material/DeblurRounded";
+import WhatshotIcon from "@mui/icons-material/Whatshot";
+import LocalMallOutlinedIcon from "@mui/icons-material/LocalMallOutlined";
+import MusicNoteOutlinedIcon from "@mui/icons-material/MusicNoteOutlined";
+import MovieCreationOutlinedIcon from "@mui/icons-material/MovieCreationOutlined";
+import LiveTvOutlinedIcon from "@mui/icons-material/LiveTvOutlined";
+import SportsEsportsOutlinedIcon from "@mui/icons-material/SportsEsportsOutlined";
+import NewspaperOutlinedIcon from "@mui/icons-material/NewspaperOutlined";
+import EmojiEventsOutlinedIcon from "@mui/icons-material/EmojiEventsOutlined";
+import LightbulbOutlinedIcon from "@mui/icons-material/LightbulbOutlined";
+import DiamondOutlinedIcon from "@mui/icons-material/DiamondOutlined";
+import PodcastsOutlinedIcon from "@mui/icons-material/PodcastsOutlined";
+import SettingsOutlinedIcon from "@mui/icons-material/SettingsOutlined";
+import OutlinedFlagOutlinedIcon from "@mui/icons-material/OutlinedFlagOutlined";
+import HelpOutlineOutlinedIcon from "@mui/icons-material/HelpOutlineOutlined";
+import AnnouncementOutlinedIcon from "@mui/icons-material/AnnouncementOutlined";
 
 const Sidebar = () => {
   return (
-    <div className='w-[14%] bg-slate-300 p-4'>
-      <div className='p-4' style={{borderBottom:"1px solid #f0f0f0"}}>
-        <ul className=''>
-          <li><HomeIcon /> Home</li>
-          <li><VideoStableOutlinedIcon /> Shorts</li>
-          <li><SubscriptionsOutlinedIcon /> Subscriptions</li>
+    <div className="w-[14%] py-2 px-2 h-screen overflow-y-scroll">
+      <div className="" style={{ borderBottom: "1px solid #f0f0f0" }}>
+        <ul className="">
+          <li className="custom-list-item">
+            <HomeIcon fontSize="medium" />
+            <span className="ml-4">Home</span>
+          </li>
+          <li className="custom-list-item">
+            <VideoStableOutlinedIcon fontSize="medium" /> <span>Shorts</span>
+          </li>
+          <li className="custom-list-item">
+            <SubscriptionsOutlinedIcon fontSize="medium" />{" "}
+            <span>Subscriptions</span>
+          </li>
         </ul>
       </div>
-      <div className='' style={{borderBottom:"1px solid #f0f0f0"}}>
+      <div style={{ borderBottom: "1px solid #f0f0f0" }}>
         <ul>
-          <li>You <KeyboardArrowRightOutlinedIcon /></li>
-          <li><HistoryOutlinedIcon /> History</li>
-          <li><PlaylistPlayOutlinedIcon /> Playlist</li>
-          <li><SmartDisplayOutlinedIcon /> Your videos</li>
-          <li><WatchLaterOutlinedIcon /> Watch Later</li>
-          <li><ThumbUpOutlinedIcon /> Liked videos</li>
+          <li className="flex custom-list-item">
+            <span className="font-semibold">You</span> <KeyboardArrowRightOutlinedIcon />
+          </li>
+          <li className="custom-list-item">
+            <HistoryOutlinedIcon />
+            <span className="ml-4">History</span>
+          </li>
+          <li className="custom-list-item">
+            <PlaylistPlayOutlinedIcon />
+            <span className="ml-4">Playlist</span>
+          </li>
+          <li className="custom-list-item">
+            <SmartDisplayOutlinedIcon />
+            <span className="ml-4">Your videos</span>
+          </li>
+          <li className="custom-list-item">
+            <WatchLaterOutlinedIcon />
+            <span className="ml-4">Watch Later</span>
+          </li>
+          <li className="custom-list-item">
+            <ThumbUpOutlinedIcon />
+            <span className="ml-4">Liked videos</span>
+          </li>
         </ul>
       </div>
-      <div className='' style={{borderBottom:"1px solid #f0f0f0"}}>
-        <h3>Subscriptions</h3>
+      <div style={{ borderBottom: "1px solid #f0f0f0" }}>
+        <h3 className="my-1 px-4 py-2 flex items-center justify-start font-semibold">Subscriptions</h3>
         <ul>
-          <li><DeblurRoundedIcon /> Cahnnel Name</li>
-          <li><DeblurRoundedIcon /> Cahnnel Name</li>
-          <li><DeblurRoundedIcon /> Cahnnel Name</li>
-          <li><DeblurRoundedIcon /> Cahnnel Name</li>
-          <li><DeblurRoundedIcon /> Cahnnel Name</li>
-          <li><DeblurRoundedIcon /> Cahnnel Name</li>
+          <li className="custom-list-item">
+            <DeblurRoundedIcon />
+            <span className="ml-4">Channel Name</span>
+          </li>
+          <li className="custom-list-item">
+            <DeblurRoundedIcon />
+            <span className="ml-4">Channel Name</span>
+          </li>
+          <li className="custom-list-item">
+            <DeblurRoundedIcon />
+            <span className="ml-4">Channel Name</span>
+          </li>
+          <li className="custom-list-item">
+            <DeblurRoundedIcon />
+            <span className="ml-4">Channel Name</span>
+          </li>
+          <li className="custom-list-item">
+            <DeblurRoundedIcon />
+            <span className="ml-4">Channel Name</span>
+          </li>
+          <li className="custom-list-item">
+            <DeblurRoundedIcon />
+            <span className="ml-4">Channel Name</span>
+          </li>
         </ul>
       </div>
-      <div className='' style={{borderBottom:"1px solid #f0f0f0"}}>
-        <h3>Explore</h3>
+      <div style={{ borderBottom: "1px solid #f0f0f0" }}>
+        <h3 className="my-1 px-4 py-2 flex items-center justify-start font-semibold">Explore</h3>
         <ul>
-          <li><WhatshotIcon /> Trending</li>
-          <li><LocalMallOutlinedIcon /> Shopping</li>
-          <li><MusicNoteOutlinedIcon /> Music</li>
-          <li><MovieCreationOutlinedIcon />Films</li>
-          <li><LiveTvOutlinedIcon /> Live</li>
-          <li><SportsEsportsOutlinedIcon /> Gaming</li>
-          <li><NewspaperOutlinedIcon />News</li>
-          <li><EmojiEventsOutlinedIcon /> Sport</li>
-          <li><LightbulbOutlinedIcon /> Courses</li>
-          <li><DiamondOutlinedIcon /> Fashion & beauty</li>
-          <li><PodcastsOutlinedIcon /> Podcasts</li>
+          <li className="custom-list-item">
+            <WhatshotIcon />
+            <span className="ml-4">Trending</span>
+          </li>
+          <li className="custom-list-item">
+            <LocalMallOutlinedIcon />
+            <span className="ml-4">Shopping</span>
+          </li>
+          <li className="custom-list-item">
+            <MusicNoteOutlinedIcon />
+            <span className="ml-4">Music</span>
+          </li>
+          <li className="custom-list-item">
+            <MovieCreationOutlinedIcon />
+            <span className="ml-4">Films</span>
+          </li>
+          <li className="custom-list-item">
+            <LiveTvOutlinedIcon />
+            <span className="ml-4">Live</span>
+          </li>
+          <li className="custom-list-item">
+            <SportsEsportsOutlinedIcon />
+            <span className="ml-4">Gaming</span>
+          </li>
+          <li className="custom-list-item">
+            <NewspaperOutlinedIcon />
+            <span className="ml-4">News</span>
+          </li>
+          <li className="custom-list-item">
+            <EmojiEventsOutlinedIcon />
+            <span className="ml-4">Sport</span>
+          </li>
+          <li className="custom-list-item">
+            <LightbulbOutlinedIcon />
+            <span className="ml-4">Courses</span>
+          </li>
+          <li className="custom-list-item">
+            <DiamondOutlinedIcon />
+            <span className="ml-4">Fashion & beauty</span>
+          </li>
+          <li className="custom-list-item">
+            <PodcastsOutlinedIcon />
+            <span className="ml-4">Podcasts</span>
+          </li>
         </ul>
       </div>
-      <div className='' style={{borderBottom:"1px solid #f0f0f0"}}>
-        <h3>More from YouTube</h3>
+      <div style={{ borderBottom: "1px solid #f0f0f0" }}>
+        <h3 className="my-1 px-4 py-2 flex items-center justify-start font-semibold">More from YouTube</h3>
         <ul>
-          <li className='flex'><img src='/icons/youtubeLogo.png' alt='youtubeLogo' /> YouTube Premium</li>
-          <li className='flex items-center'><img className='w-9 h-9' src='/icons/youtubeStudioLogo.png' alt='youtubeLogo' />YouTube Studio</li>
-          <li className='flex'><img src='/icons/youtubeMusicLogo.png' alt='youtubeLogo' />YouTube Music</li>
-          <li className='flex'><img src='/icons/youtubeKidsLogo.png' alt='youtubeLogo' />YouTube Kids</li>
+          <li className="flex custom-list-item">
+            <img src="/icons/youtubeLogo.png" alt="youtubeLogo" />{" "}
+            <span className="ml-4">YouTube Premium</span>
+          </li>
+          <li className="flex custom-list-item">
+            <img
+              className="w-6 h-6"
+              src="/icons/youtubeStudioLogo.png"
+              alt="youtubeLogo"
+            />
+            <span className="ml-4">YouTube Studio</span>
+          </li>
+          <li className="flex custom-list-item">
+            <img src="/icons/youtubeMusicLogo.png" alt="youtubeLogo" />
+            <span className="ml-4">YouTube Music</span>
+          </li>
+          <li className="flex custom-list-item">
+            <img src="/icons/youtubeKidsLogo.png" alt="youtubeLogo" />
+            <span className="ml-4">YouTube Kids</span>
+          </li>
         </ul>
       </div>
-      <div className='' style={{borderBottom:"1px solid #f0f0f0"}}>
+      <div className="" style={{ borderBottom: "1px solid #f0f0f0" }}>
         <ul>
-          <li><SettingsOutlinedIcon /> Sttings</li>
-          <li><OutlinedFlagOutlinedIcon />Report History</li>
-          <li><HelpOutlineOutlinedIcon /> Help</li>
-          <li><AnnouncementOutlinedIcon /> Send Feedback</li>
+          <li className="flex custom-list-item">
+            <SettingsOutlinedIcon />
+            <span className="ml-4">Settings</span>
+          </li>
+          <li className="flex custom-list-item">
+            <OutlinedFlagOutlinedIcon />
+            <span className="ml-4">Report History</span>
+          </li>
+          <li className="flex custom-list-item">
+            <HelpOutlineOutlinedIcon />
+            <span className="ml-4">Help</span>
+          </li>
+          <li className="flex custom-list-item">
+            <AnnouncementOutlinedIcon />
+            <span className="ml-4">Send Feedback</span>
+          </li>
         </ul>
       </div>
-      <div className='text-[#606060]'>
-        <p>AboutPressCopyrightContact usCreatorAdvertiseDevelopers</p>
-        <p>TermsPrivacyPolicy & SafetyHow YouTube worksTest new features</p>
-        <p>© 2024 Google LLC</p>
+      <div className="text-[#606060] p-4">
+        <p className="text-sm font-bold">About Press Copyright Contact us Creator Advertise Developers</p>
+        <p className="text-sm font-bold py-2">Terms Privacy Policy & Safety How YouTube works Test new features</p>
+        <p className="text-xs py-2">© 2024 Google LLC</p>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Sidebar
+export default Sidebar;
